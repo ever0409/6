9206b254648111f327d640f60bb540f30b9126eb
ever0409 <sajad0409@gmail.com>
2016-10-05 23:51:03 +0330

    Update getplug.lua

--- plugins/getplug.lua
+++ plugins/getplug.lua
@@ -1,18 +1,19 @@
 do
 
 local function run(msg, matches)
-  if matches[1] == "getplug" then
+  if matches[1] == "send" then
     local file = matches[2]
     if is_sudo(msg) then
       local receiver = get_receiver(msg)
       send_document(receiver, "./plugins/"..file..".lua", ok_cb, false)
+      return "❗پلاگین "..file.." ارسال شد!"
     end
   end
 end
 
 return {
   patterns = {
-  "^#(getplug) (.*)$"
+  "^[!/#](send) (.*)$"
   },
   run = run
 }
