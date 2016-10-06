6c03e34f34d59fa87fe40e26a4b5084238cc2546
ever0409 <sajad0409@gmail.com>
2016-10-07 00:51:20 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,14 +1,14 @@
-do
-
 function run(msg, matches)
-    return "Self-Bot v2 \n by BeatBot Team :) <3 \n @BeatBot_Team"
+    return "❗▶Self BoT⏬\nPersian And English Supported By SelfBoT⏬\nCreate By Mr.Nitro⏬\nBase is Beat BoT And mr.nitro self NoW◀❗"
 end 
 return {
   patterns = {
-    "^#version$",
-    "^#bot$",
-    "^#selfbot$"
+    "^version$",
+    "^(ورژن)$",
+    "^bot$",
+    "^([Aa][Bb][Oo][Uu][Tt])$",
+    "^(درباره)$",
+    "^selfbot$"
   },
   run = run
 }
-end
